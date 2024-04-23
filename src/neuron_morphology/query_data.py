bfd4c2c2ca9a9e8f43e0b60ddf0559ba8d41e7a5
mouffok <sarah.mouffok@epfl.ch>
2024-04-23 09:16:32 +0200

    support for querying only morphologies that are not curated

--- src/neuron_morphology/query_data.py
+++ src/neuron_morphology/query_data.py
@@ -1,21 +1,21 @@
 from kgforge.core import KnowledgeGraphForge
+from kgforge.core.wrappings import FilterOperator, Filter
 
 
 def get_neuron_morphologies(forge: KnowledgeGraphForge, reconstructed=True, curated="yes", limit=10000, debug=False):
+    type_ = "ReconstructedNeuronMorphology" if reconstructed else "NeuronMorphology"
+    filters = [Filter(operator=FilterOperator.EQUAL, path=["type"], value=type_)]
 
-    q = {"type": "ReconstructedNeuronMorphology" if reconstructed else "NeuronMorphology"}
-
-    if curated == "yes":
-        q["annotation"] = {
-            "hasBody": {
-                "label": "Curated"
-            }
-        }
-    elif curated == "no":
-        raise NotImplemented()  # TODO filter that curated flag shouldn't be there
+    if curated in ["yes", "no"]:
+        filters.append(
+            Filter(
+                operator=FilterOperator.EQUAL if curated == "yes" else FilterOperator.NOT_EQUAL,
+                path=["annotation", "hasBody", "label"], value="Curated"
+             )
+        )
     elif curated == "both":
         pass
     else:
         raise Exception(f"Unknown curated flag when retrieving neuron morphologies {curated}")
 
-    return forge.search(q, limit=limit, debug=debug)
+    return forge.search(*filters, limit=limit, debug=debug)
