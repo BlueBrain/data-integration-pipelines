33a00ed5f4be63b65085ece7ea97fabf7d3edaf7
mouffok <sarah.mouffok@epfl.ch>
2024-09-19 10:51:07 +0200

    issue with update of construct query

--- src/search_index/the_issue.py
+++ src/search_index/the_issue.py
@@ -49,6 +49,23 @@ def compare_sp_weirdness(token: str, endpoint: str):
 
         return q % type_
 
+
+    def _make_sp_query5():
+        q = """
+            SELECT ?id
+            WHERE {
+              ?id a <%s> .
+              ?id <https://neuroshapes.org/annotation>/<https://neuroshapes.org/hasBody>/<http://www.w3.org/2000/01/rdf-schema#label>  "Curated" .
+              OPTIONAL {
+                    ?id a <https://bbp.epfl.ch/ontologies/core/bmo/ExperimentalTrace> .
+                    FILTER EXISTS {?id <http://schema.org/distribution>/<http://schema.org/encodingFormat>  "application/nwb"} .
+              } .
+            }
+            LIMIT 10000
+        """
+
+        return q % type_
+
     def _make_sp_query2():
         q = """
             SELECT ?id
@@ -72,8 +89,9 @@ def compare_sp_weirdness(token: str, endpoint: str):
     queries = [
         _make_sp_query(brain_regions=None, type_=type_, curated_flag=True),  # No filter on encoding format
         _make_sp_query2(),  # What should be - but cannot be expressed like that
-        _make_sp_query3(),  # The current state
-        _make_sp_query4()   # The fix
+        _make_sp_query3(),  # The old state, filtering too much
+        _make_sp_query4(),   # The initial fix
+        _make_sp_query5()  # The updated fix due to performance issues. Doesn't filter out the 2 traces w/o distribution
     ]
 
     responses = [
@@ -85,6 +103,8 @@ def compare_sp_weirdness(token: str, endpoint: str):
         set(i["id"]['value'] for i in resp['results']['bindings'])
         for resp in responses
     ]
+
+    print(res[4].difference(res[3]))
 
     print([len(res_i) for res_i in res])
 
